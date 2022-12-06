7e962ecae7e436e7f5065c132214cdb37bbcfd62
Katenver <8711724@ukr.net>
2022-12-06 18:28:37 +0200

    const instead let

--- src/js/simpleLightBox.js
+++ src/js/simpleLightBox.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export let simpleGallery = new SimpleLightbox('.gallery a', {
+export const simpleGallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   loop: true,
   captionDelay: 300,
